9d8eabc0f94f73e7551a34de46491d759e6de1bd
ankitaaKJ <ankitajamdade413@gmail.com>
2025-09-03 01:54:05 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,9 @@
 def call(Map config = [:]) {
     loadLinuxScript(name: 'Dockerfile')
     sh "docker build -t myapacheimg ."
-    dockerRun(name: 'myApacheImg')
+    dockerRun(name: 'myapacheimg:latest')
 }
+
 
 
 
